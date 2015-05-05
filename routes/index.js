ba7158ca1f8fd568877758b87e5b0cf455e21a93
Hermal Ludovic <lhermal@etu.unistra.fr>
2015-05-05 16:04:18 +0200

    commit nécessaire

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var tweets = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', tweets: tweets });
+  res.render('index', { title: 'miniTwr', tweets: tweets });
 });
 
 router.post('/', function(req, res, next) {
