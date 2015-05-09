e993b889b92607900afce5d750481c0f9d5a04a2
HERMAL Ludovic <Ludovic.Hermal@gmail.com>
2015-05-09 13:37:02 +0200

    Ajout des routes pour les pages Options, Chat, Enregistrement et Profil

--- app.js
+++ app.js
@@ -29,7 +29,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.get('/octochat', site.homePage);
+app.get('/octochat', site.chat);
+app.get('/profil', site.profil);
+app.get('/option', site.option);
+app.get('/enregistrement', site.enregistrement);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/site.js
+++ routes/site.js
@@ -1,7 +1,15 @@
-/*
- * GET real home page.
- */
-
-exports.homePage = function(req, res){
+exports.chat = function(req, res){
   res.render('octochat', { title: 'Octochat - miniTwr' });
+};
+
+exports.profil = function(req, res){
+  res.render('profil', { title: 'Profil - miniTwr' });
+};
+
+exports.option = function(req, res){
+  res.render('option', { title: 'Options - miniTwr' });
+};
+
+exports.enregistrement = function(req, res){
+  res.render('enregistrement', { title: 'Enregistrement - miniTwr' });
 };
